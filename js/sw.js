659cd9767400741a2638075c0dac3f1d0c0212d6
semoozaher-wq <semoo.zaher@gmail.com>
2026-04-28 18:43:38 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,20 +1,35 @@
 const CACHE_NAME = 'semo-erp-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/dashboard.html',
-    '/main.js',
-    '/style.css'
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&display=swap',
+  'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
